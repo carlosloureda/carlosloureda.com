4f238ac497688ff899c347812ea2cddea6e46004
Carlos Loureda <carloslouredaparrado@gmail.com>
2021-02-18 10:21:47 +0100

    feat: Starter blog template for mdx files (#1)
    * feat: Copied most of the code from kent´s code.
    
    Let´s try to make this work, try to have some quick mdx blogs
    methods to build our spanish blog.
    Also we will beed to add a home  page soon to have a "basic" starter.
    
    * fix: Fixed blogs from mdx make them work
    
    * feat: Added 2nd possible blog title
    
    * fix:Fixed main header and foot page
    
    * feat: fixed hader for blog index page
    
    * fix: Several fixes
    
    * feat: Generate blog post from Kent C added
    
    * fix: Removed the default blog post

---
+++ config/website.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteTitle: "Carlos Loureda", // Navigation and Site Title
+  siteTitleAlt: "La web personal de Carlos Loureda", // Alternative Site title for SEO
+  siteTitleShort: "carlosloureda", // short_name for manifest
+  siteUrl: process.env.ROOT_URL || "https://carlosloureda.com", // Domain of your site. No trailing slash!
+  lang: "es", // Language Tag on <html> element
+  pathPrefix: "/",
+  siteLogo: "images/logo.png", // Used for SEO and manifest, path to your image you placed in the 'static' folder
+  siteDescription: "Ven a aprender conmigo React  JS.",
+  minibio: `
+      <strong>Carlos Loureda</strong> está creando contenido en React y Gatsby.
+    `,
+  author: "Carlos Loureda", // Author for schemaORGJSONLD
+  organization: "Carlos Loureda Tech LLC",
+
+  // siteFBAppID: '123456789', // Facebook App ID - Optional
+  userTwitter: "@carlosloureda", // Twitter Username
+  ogSiteName: "Carlos Loureda", // Facebook Site Name
+  ogLanguage: "en_US",
+
+  // Manifest and Progress color
+  themeColor: "#4147DC",
+  backgroundColor: "#231C42",
+
+  // Social component
+  twitter: "https://twitter.com/carlosloureda/",
+  twitterHandle: "@carlosloureda",
+  github: "https://github.com/carlosloureda/",
+  linkedin: "https://www.linkedin.com/in/carlosloureda/",
+  youtube: "https://www.youtube.com/channel/UCQecfidvV4U8EKkH7iR1Dsg",
+  // TODO:make this work
+  rss: "https://carlosloureda.com/blog/rss.xml",
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,49 @@
+const path = require("path");
+const config = require("./config/website");
+
+const {
+  NODE_ENV,
+  URL: NETLIFY_SITE_URL = config.siteUrl,
+  DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+  CONTEXT: NETLIFY_ENV = NODE_ENV,
+} = process.env;
+const isNetlifyProduction = NETLIFY_ENV === "production";
+const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+
+const here = (...p) => path.join(__dirname, ...p);
+
+require("dotenv").config({
+  path: `.env.${NODE_ENV}`,
+});
+
 module.exports = {
   siteMetadata: {
-    title: "My Gatsby Site",
+    siteUrl,
+    title: config.siteTitle,
+    twitterHandle: config.twitterHandle,
+    description: config.siteDescription,
+    keywords: [
+      "Desarrollo Software",
+      "Formación React",
+      "Formación Javascript",
+      "React Training",
+      "JavaScript Training",
+    ],
+    canonicalUrl: siteUrl,
+    image: config.siteLogo,
+    author: {
+      name: config.author,
+      minibio: config.minibio,
+    },
+    organization: {
+      name: config.organization,
+      url: siteUrl,
+      logo: config.siteLogo,
+    },
+    social: {
+      twitter: config.twitterHandle,
+      fbAppID: "",
+    },
   },
   plugins: [
     "gatsby-plugin-emotion",
@@ -20,7 +63,41 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-plugin-mdx",
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          default: here("./src/templates/markdown-page.js"),
+        },
+        extensions: [".mdx", ".md", ".markdown"],
+        gatsbyRemarkPlugins: [
+          { resolve: "gatsby-remark-copy-linked-files" },
+          {
+            resolve: "gatsby-remark-autolink-headers",
+            options: {
+              icon: `<svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M14.851 11.923c-.179-.641-.521-1.246-1.025-1.749-1.562-1.562-4.095-1.563-5.657 0l-4.998 4.998c-1.562 1.563-1.563 4.095 0 5.657 1.562 1.563 4.096 1.561 5.656 0l3.842-3.841.333.009c.404 0 .802-.04 1.189-.117l-4.657 4.656c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-1.952-1.951-1.952-5.12 0-7.071l4.998-4.998c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464.493.493.861 1.063 1.105 1.672l-.787.784zm-5.703.147c.178.643.521 1.25 1.026 1.756 1.562 1.563 4.096 1.561 5.656 0l4.999-4.998c1.563-1.562 1.563-4.095 0-5.657-1.562-1.562-4.095-1.563-5.657 0l-3.841 3.841-.333-.009c-.404 0-.802.04-1.189.117l4.656-4.656c.975-.976 2.256-1.464 3.536-1.464 1.279 0 2.56.488 3.535 1.464 1.951 1.951 1.951 5.119 0 7.071l-4.999 4.998c-.975.976-2.255 1.464-3.535 1.464-1.28 0-2.56-.488-3.535-1.464-.494-.495-.863-1.067-1.107-1.678l.788-.785z"/></svg>`,
+              enableCustomId: true,
+            },
+          },
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              backgroundColor: "#fafafa",
+              maxWidth: 1035,
+            },
+          },
+          // {
+          //   resolve: `gatsby-remark-embedder`,
+          //   options: {
+          //     customTransformers: [eggheadTransformer],
+          //   },
+          // },
+          // {
+          //   resolve: require.resolve("./other/gatsby-remark-af"),
+          // },
+        ],
+      },
+    },
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-source-filesystem",
@@ -37,6 +114,20 @@ module.exports = {
         path: "./src/pages/",
       },
       __key: "pages",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: "blog",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        path: `${__dirname}/src`,
+        name: "src",
+      },
     },
   ],
 };
